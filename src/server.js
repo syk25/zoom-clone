437f793425d8d302533b9c517e3dc1c92ae2b20c
syk25 <syk2245@gmail.com>
2024-11-25 07:29:44 +0900

    사용자가 home만을 보게끔 설정

--- src/server.js
+++ src/server.js
@@ -7,12 +7,16 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public")); // 사용자가 볼 수 있는 폴더를 명시적으로 설정
+
 app.get("/", (req, res) => {
     res.render("home");
 });
 
-console.log("Hello");
+// 다른 경로로 접근 시 home을 보여줌
+app.use("/*", (erq, res) => {
+    res.render("home");
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on ${SERVER}`);
