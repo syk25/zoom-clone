a8f8556195811295f2603edeb9857af99fe5ef92
syk25 <syk2245@gmail.com>
2024-11-25 07:26:08 +0900

    front setup

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 import express from "express";
 
 const PORT = 3000;
+const SERVER = `http://localhost:${PORT} 🚀`;
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
 console.log("Hello");
 
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`Listening on ${SERVER}`);
+});
